2fa1c0dc2e51118fa7db41d447fe9c9b4021e068
parsalotfy <parsalotfy@outlook.com>
2020-05-24 02:29:07 +0430

    Add L2TP_Fetcher project

---
+++ L2TP_Fetcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L2TP_Fetcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
